b00ab595ed98837498a15e94a8533d079e840e10
Aneesh <aneesh.chan@gmail.com>
2019-10-03 01:18:04 -0500

    Add repo

--- hello/index.js
+++ hello/index.js
@@ -2,12 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const token = core.getInput('repo-token');
-    const requestEvent = core.getInput('event');
-    const body = core.getInput('body');
-    console.log(`token is ${token}`);
+    const repo = github.context.repo
+    const requestEvent  = JSON.stringify(core.getInput('event'), undefined, 2);
+    const token  = JSON.stringify(core.getInput('repo-token'), undefined, 2);
+    const body  = JSON.stringify(core.getInput('body'), undefined, 2);
+    console.log(`repo is ${repo}`);
     console.log(`requestEvent is ${requestEvent}`);
-    console.log(`requestEvent is ${body}`);
+    console.log(`body is ${body}`);
     // `who-to-greet` input defined in action metadata file
     const environment = core.getInput("env");
     console.log(environment);
@@ -17,7 +18,7 @@ try {
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
