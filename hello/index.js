9f774c8fc7c4e77d3b612b5fbb5415c53413081a
Aneesh <aneesh.chan@gmail.com>
2019-10-03 22:11:39 -0500

    copy file from anu repo

--- hello/index.js
+++ hello/index.js
@@ -1,38 +1,61 @@
+const Octokit = require("@octokit/rest");
 const core = require('@actions/core');
 const github = require('@actions/github');
+
 const { Toolkit } = require('actions-toolkit');
-const tools = new Toolkit()
+const toolkit = new Toolkit();
+
+const octokit = new Octokit({
+    auth: '${token}'
+});
 
 try {
-    const repo = github.context.repo;
-    const requestEvent  = JSON.stringify(core.getInput('event'), undefined, 2);
-
-    const  token = tools.token;
+    const repo = process.env.GITHUB_REPOSITORY.toString();
+    var repoNameWithOwnerArray = repo.split("/", 2);
+    const owner = repoNameWithOwnerArray[0];
+    const actualRepo = repoNameWithOwnerArray[1];
+    console.log(`Hello owner ${owner}!`);
+    console.log(`Hello  repo ${actualRepo}!`);
+    const pull_number = core.getInput('pull_number');
+    console.log(`Hello pull_number ${pull_number}!`);
     const body  = JSON.stringify(core.getInput('body'), undefined, 2);
-    const str = repo.repo.toString();
-    const owner = repo.owner.toString();
-    const eventName = github.context.eventName.toString();
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`repo as owner is ${owner}`);
-    console.log(`repo as string is ${str}`);
-    console.log(`repo as eventName is ${eventName}`);
-    console.log(`Token is ${token}`)
-
-
-
-
-
-    // `who-to-greet` input defined in action metadata file
-    const environment = core.getInput("env");
-    console.log(environment);
-    console.log(`Environment is ${environment}`);
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    console.log(`Hello body ${body}!`);
+    const commit_id = process.env.GITHUB_SHA;
+    console.log(`Hello commit_id ${commit_id}!`);
+    const path = core.getInput('path');
+    console.log(`Hello path ${path}!`);
+    const position = core.getInput('position');
+    console.log(`Hello position ${position}!`);
+    const token = process.env.TOKEN.toString();
+    console.log(`Hello token ${token}`);
     // Get the JSON webhook payload for the event that triggered the workflow
-
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+    const headref = process.env.GITHUB_HEAD_REF.toString();
+    const baseref = process.env.GITHUB_BASE_REF.toString();
+    console.log(`hello headref ${headref}`);
+    console.log(`hello baseref ${baseref}`);
+
+    const host = process.env.GITHUB_HOST_SUFFIX.toString();
+    console.log(`###############yes! this is the host ${host}`);
+    const parent = process.env.GITHUB_PARENT_SPACE.toString();
+    console.log(`###############yes! this is the parent ${parent}`);
+    if("dev"==parent)
+    {
+        console.log(`###############yes! this is the parent space secret`);
+    }
+    const bodyprime = `http://${headref}.s/${parent}.bikesharingweb.${host}/`;
+    console.log(`###############yes! this is the bodyprime ${bodyprime}`);
+    octokit.pulls.createComment({
+        owner: '${owner}',
+        repo: '${actualRepo}',
+        pull_number: '${pull_number}',
+        body: '${bodyprime}',
+        commit_id: '${commit_id}',
+        path: '${path}',
+        position: '${position}'
+    });
 } catch (error) {
     core.setFailed(error.message);
 }
+
